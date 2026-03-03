59a4ba1075a60c102cb2f7598f55291dd0a73e23
Senestres <100228689+Senestres@users.noreply.github.com>
2026-03-03 21:18:03 +0100

    symlink test

--- content/fr/entrainement/entrainement.11tydata.js
+++
@@ -1,9 +0,0 @@
-export default {
-	categories: "forme d'entrainement",
-	tags: [
-		"posts",
-		"entrainement"
-	],
-	"layout": "layouts/post.njk",
-	"permalink": "{{ lang }}/entrainement/{{ title | slugify }}/",
-};
--- content/fr/mouvement/mouvement.11tydata.js
+++
@@ -1,9 +0,0 @@
-export default {
-	categories: "forme de mouvement",
-	tags: [
-		"posts",
-		"mouvement",
-	],
-	"layout": "layouts/post.njk",
-	"permalink": "{{ lang }}/mouvement/{{ title | slugify }}/",
-};
