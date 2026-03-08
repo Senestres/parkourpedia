d3cf76fa744bd14ae54e2b912b3467592254afc1
Senestres <100228689+Senestres@users.noreply.github.com>
2026-03-08 07:59:58 +0100

    untrack en

--- content/en/mouvement/mouvement.11tydata.js
+++
@@ -1,9 +0,0 @@
-export default {
-	categories: "forme de mouvement",
-	tags: [
-		"posts",
-		"mouvement",
-	],
-	"layout": "layouts/post.njk",
-	"permalink": "{{ lang }}/{{ 'mouvement' | t(lang) }}/{{ title | slugify }}/",
-};
