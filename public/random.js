093fed1edf5b2e7c52986a7c84880bb3b6af8785
Senestres <100228689+Senestres@users.noreply.github.com>
2026-03-10 17:21:31 +0100

    add randomize button

---
+++ public/random.js
@@ -0,0 +1,29 @@
+function renderRandomPost(dataId, cardId) {
+  const posts = JSON.parse(document.querySelector(dataId).textContent);
+  const post = posts[Math.floor(Math.random() * posts.length)];
+  const card = document.querySelector(cardId);
+
+  card.querySelector("a").setAttribute("href", post.url);
+  card.querySelector(".card-overlay").textContent = post.title || post.url;
+
+  const img = card.querySelector(".card-img");
+  if (post.coverImage || post.video) {
+    img.src = post.coverImage || post.video;
+    img.style.display = "";
+  }
+}
+renderRandomPost("#move-posts", "#random-move-card");
+renderRandomPost("#training-posts", "#random-training-card");
+
+document.addEventListener("click", (e) => {
+  const btn = e.target.closest(".randomize");
+  if (!btn) return;
+
+  renderRandomPost(
+    btn.dataset.source,
+    btn.dataset.target
+  );
+});
+  
+
+
