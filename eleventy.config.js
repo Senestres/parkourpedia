2fcf7a2bde3b0a06a83109d5235e03087ec86728
Senestres <100228689+Senestres@users.noreply.github.com>
2026-03-14 14:35:52 +0100

    rm embedEverything

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,6 @@ import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import Image from "@11ty/eleventy-img";
-import embedEverything from "eleventy-plugin-embed-everything";
 import markdownItFootnote from 'markdown-it-footnote';
 import { execSync } from 'child_process';
 import { I18nPlugin } from "@11ty/eleventy";
